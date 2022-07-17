4e215225b7570aa31c3c290a00488868131e855c
ArtemHorbas <artemgorbas95@gmail.com>
2022-07-17 23:23:15 +0200

    third commit

--- src/MyAxios.js
+++ src/MyAxios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const  instance = axios.create({
-	baseURL: process.env.REACT_APP_API_URL
+	baseURL: "http://localhost:5454"
 })
+
+//process.env.REACT_APP_API_URL
 
 instance.interceptors.request.use(config => {
 	config.headers.Authorization = window.localStorage.getItem('token')
